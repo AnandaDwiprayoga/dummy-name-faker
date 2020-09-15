d3dc09bb6172ef52c7985ae4cd3c56d372432024
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-09-15 18:35:12 +0700

    add test coverage

--- src/all.test.js
+++ src/all.test.js
@@ -1,10 +1,12 @@
-const all = require('./data/names.json');
+const all = require("./all");
 
 
 test('make sure all item in array is string', () => {
-   const isAllDataString = all.every(val => {
+   const isAllDataString = all().every(val => {
        return typeof val === 'string';
    })
 
    expect(isAllDataString).toBe(true);
 });
+
+
