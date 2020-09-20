bed048d469ff7a13bf80196bdc44a18650f5b65b
AnandaDwiPrayoga <Prayogaananda04@gmail.com>
2020-09-20 19:43:02 +0700

    change eslint config to module type

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
+    es6: true,
     node: true,
     jest: true
   },
   extends: "eslint:recommended",
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 6,
+    sourceType: "module"
   },
   rules: {
     semi: ["error", "always"],
